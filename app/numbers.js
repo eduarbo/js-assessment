e14395261d4e0adfa4aa33df4e94a7ad7c931bca
Eduardo Ruiz <eduarbo@gmail.com>
2013-02-05 14:47:03 -0600

    Finished excercises for numbers

--- app/numbers.js
+++ app/numbers.js
@@ -3,19 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+      var binNum = num.toString(2);
+      return +binNum[binNum.length - bit]
     },
 
     base10: function(str) {
-
+      return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
-
+      var bin = parseFloat(num).toString(2);
+      return Array( 9 - bin.length ).join('0') + bin;
     },
 
     multiply: function(a, b) {
-
+      return a * 1e10 * b * 1e10 / 1e20
     }
   };
 });
