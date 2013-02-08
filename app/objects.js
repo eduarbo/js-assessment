56efd684c4df503850c3f6183692b369747d44f2
Eduardo Ruiz <eduarbo@gmail.com>
2013-02-08 06:06:56 -0600

    done with objects

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,20 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.apply(obj)
     },
 
     alterObjects : function(constructor, greeting) {
-
+      return constructor.prototype.greeting = greeting
     },
 
     iterate : function(obj) {
+      var keys = [];
+      for ( var i  in obj ) {
+        if( obj.hasOwnProperty(i) ) keys.push( i + ": " + obj[i] )
+      }
 
+      return keys;
     }
   };
 });
